235638c1d11a41ef1590b05e8e35f2150c9dc503
Jack Broadway <jack.c.broadway@gmail.com>
2016-11-13 20:04:58 +1000

    Added basic deps

---
+++ app.js
@@ -0,0 +1,7 @@
+//NPM Packages
+var express = require("express");
+var https = require("https");
+var logger = require('morgan');
+var winston = require('winston');
+var fs = require('fs');
+//App Components
