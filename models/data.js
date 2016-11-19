f1ca78ac5c8987d02aa010b4588f34c48a23d59b
Jack Broadway <jack.c.broadway@gmail.com>
2016-11-19 11:53:18 +1000

    Added sensor and data models

---
+++ models/data.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const uuid = require('node-uuid');
+//config
+const config = require('../config/config.js');
+//Model Setup
+const Sensor = require('./sensor');
+const Schema = mongoose.Schema;
+
+var DataSchema = new Schema({
+    recorded_at: {type: Date, required: true, default: Date.now},
+    sensorId: {type: String, required: true},
+    sensorData: {type: String, required: true}
+});
+DataSchema.pre("save", function(next){
+    var datapoint = this;
+    Sensor.count({_id: datapoint.sensorId}, function(err, count){
+        if (err) return next(err);
+        if(count === 0){
+            var error = new Error("Sensor doesnt exist in db")
+            error.maccode = "sen_no_exist";
+            return next(error);
+        } else {
+             return next();
+        }
+    })
+});
+module.exports = mongoose.model('Data', DataSchema);
---
+++ models/sensor.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const uuid = require('node-uuid');
+//config
+const config = require('../config/config.js');
+//Model Setup
+const Schema = mongoose.Schema;
+
+var SensorSchema = new Schema({
+    _id: {type: String, default: uuid.v4, unique: true},
+    name: {type: String, required: true},
+    type: {type: String, required: true},
+    desc: {type: String, required: true},
+    hub: {type: String, required: true},
+});
+
+
+module.exports = mongoose.model('Sensor', SensorSchema);
