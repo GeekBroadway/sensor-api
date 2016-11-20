a54e9dacddd97f10d6a824fdc4ddad375c1ecab7
Jack Broadway <jack.c.broadway@gmail.com>
2016-11-20 11:30:33 +1000

    Made console output silent during tests

--- utils/logger.js
+++ utils/logger.js
@@ -1,38 +1,67 @@
 let winston = require('winston');
 winston.emitErrs = true;
 
-let logger = new winston.Logger({
-    levels: {
-        'debug': 0,
-        'startup': 0,
-        'info': 1,
-        'error': 4
-    },
-    colors: {
-        'debug': 'blue',
-        'startup': 'cyan',
-        'info': 'green',
-        'error': 'red'
-    },
-    transports: [
-        new winston.transports.File({
-            level: 'info',
-            filename: './logs/all-logs.log',
-            handleExceptions: true,
-            json: true,
-            maxsize: 5242880, //5MB
-            maxFiles: 5,
-            colorize: false
-        }),
-        new winston.transports.Console({
-            level: 'error',
-            handleExceptions: true,
-            json: false,
-            colorize: true
-        })
-    ],
-    exitOnError: false
-});
+let logger;
+if(process.env.NODE_ENV !== 'test') {
+    logger = new winston.Logger({
+        levels: {
+            'debug': 0,
+            'startup': 0,
+            'info': 1,
+            'error': 4
+        },
+        colors: {
+            'debug': 'blue',
+            'startup': 'cyan',
+            'info': 'green',
+            'error': 'red'
+        },
+        transports: [
+            new winston.transports.File({
+                level: 'info',
+                filename: './logs/all-logs.log',
+                handleExceptions: true,
+                json: true,
+                maxsize: 5242880, //5MB
+                maxFiles: 5,
+                colorize: false
+            }),
+            new winston.transports.Console({
+                level: 'error',
+                handleExceptions: true,
+                json: false,
+                colorize: true
+            })
+        ],
+        exitOnError: false
+    });
+} else {
+    logger = new winston.Logger({
+        levels: {
+            'debug': 0,
+            'startup': 0,
+            'info': 1,
+            'error': 4
+        },
+        colors: {
+            'debug': 'blue',
+            'startup': 'cyan',
+            'info': 'green',
+            'error': 'red'
+        },
+        transports: [
+            new winston.transports.File({
+                level: 'debug',
+                filename: './logs/testing-log.log',
+                handleExceptions: true,
+                json: true,
+                maxsize: 5242880, //5MB
+                maxFiles: 5,
+                colorize: false
+            })
+        ]
+    });
+}
 
 module.exports = logger;
 module.exports.stream = {
