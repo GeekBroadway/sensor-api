cf7ede402b2e81b544ce9a32940a1b1c672ea199
Jack Broadway <jack.c.broadway@gmail.com>
2016-11-21 23:10:35 +1000

    Added delete and get info sensor routes, and tests

--- routes/sensor.js
+++ routes/sensor.js
@@ -11,8 +11,11 @@ sensor.use(bodyParser.json());
 sensor.use(expressValidator());
 //mongoose
 let Sensor = require('../models/sensor.js');
+let Data = require('../models/data.js');
 //Routes
 sensor.post('/add', postAddSensor);
+sensor.get('/by/:UUID', getByUUID);
+sensor.delete('/by/:UUID', deleteByUUID);
 //Logic
 function postAddSensor(req, res){
     req.checkBody('name', 'Invalid sensorId').notEmpty();
@@ -42,9 +45,69 @@ function postAddSensor(req, res){
         if(err) {
             res.status(500).json({status: false, message: "DB Error occurred", errors: null});
             logger.error(err);
+            return;
         } else {
             res.json({status: true, message: "sensor added", sensorId: newSensor._id});
         }
     });
+}
+function getByUUID(req, res){
+    req.checkParams('UUID', 'Invalid sensorID').notEmpty().isUUID();
+
+    let errors = req.validationErrors();
+    if(errors) {
+        res.status(400).json({status: false, message: 'Val Errors', errors: errors});
+        return;
+    }
+    let sensorId = req.params.UUID;
+
+    Sensor.findOne({_id: sensorId}).then(function(sensor){
+        if(sensor !== null) {
+            let dataObject = {sensorId: sensorId, name: sensor.name, type: sensor.type, desc: sensor.desc, hub: sensor.hub};
+            res.json({status: true, message: "sensor data retrieved", data: dataObject});
+        } else {
+            res.status(400).json({status: false, message: "Sensor not in database", errors: null})
+        }
+    }).catch(function(err){
+        if(err){
+            res.status(500).json({status: false, message: 'DB Error Occurred', errors: null});
+            logger.error(err.message);
+        }
+    });
+}
+function deleteByUUID(req,res){
+    req.checkParams('UUID', 'Invalid sensorID').notEmpty().isUUID();
+
+    let errors = req.validationErrors();
+    if(errors) {
+        res.status(400).json({status: false, message: 'Val Errors', errors: errors});
+        return;
+    }
+    let sensorId = req.params.UUID;
+
+    Sensor.findOne({_id: sensorId}).remove().then(function(removed){
+        if(removed.result.n === 0){
+            res.status(400).json({status: false, message: "Sensor not in database", errors: null});
+            return;
+        }
+        if(removed.result.n == 1){
+            deleteData();
+        }
+    }).catch(function(err){
+        if(err) {
+            res.status(500).json({status: false, message: "DB Error Occurred", errors: null});
+            logger.error(err.message);
+        }
+    });
+    function deleteData() {
+        Data.find({sensorId: sensorId}).remove().then(function(removed){
+            res.status(200).json({status: true, message: "Sensor Deleted", dataDeleted: removed.result.n})
+        }).catch(function(err){
+            if(err) {
+                res.status(500).json({status: false, message: "DB Error Occured", errors: null})
+                logger.error(err.message);
+            }
+        });
+    }
 }
 module.exports = sensor;
--- test/sensor.js
+++ test/sensor.js
@@ -7,6 +7,9 @@ const server = require('../app.js');
 const should = chai.should();
 chai.use(chaiHttp);
 
+let UUID;
+let falseUUID = 'c5233e47-604a-435d-bf92-c2fd61defb85';
+
 describe('POST /api/sensor/add', function(){
    it('should create a sensor given correct fields', function(done){
        chai.request(server)
@@ -20,6 +23,7 @@ describe('POST /api/sensor/add', function(){
                 res.status.should.equal(200);
                 res.body.status.should.be.true;
                 res.body.should.have.all.keys('status','message','sensorId');
+                UUID = res.body.sensorId;
                 done();
        });
    });
@@ -97,5 +101,86 @@ describe('POST /api/sensor/add', function(){
             res.body.errors[1].should.have.all.keys('param','msg');
             done();
         });
+    });
+});
+describe('GET /api/sensor/by/UUID', function(){
+    it('should return true when given a valid UUID', function(done){
+       chai.request(server)
+           .get('/api/sensor/by/'+UUID)
+           .end(function(err, res){
+               res.status.should.equal(200);
+               res.body.status.should.be.true;
+               res.body.should.have.all.keys('status','message','data');
+               res.body.data.should.have.all.keys('sensorId','name','type','desc','hub');
+               done();
+       });
+    });
+    it('should return not found when given no UUID', function(done){
+        chai.request(server)
+            .get('/api/sensor/by')
+            .end(function(err, res){
+                res.status.should.equal(404);
+                done();
+            });
+    });
+    it('should error given an invalid UUID', function(done){
+        chai.request(server)
+            .get('/api/sensor/by/'+UUID+'1')
+            .end(function(err, res){
+                res.status.should.equal(400);
+                res.body.status.should.be.false;
+                res.body.should.have.all.keys('status','message','errors');
+                res.body.errors[0].should.have.all.keys('param','msg','value');
+                done();
+            });
+    });
+    it('should error when given a UUID that doesnt exist', function(done){
+        chai.request(server)
+            .get('/api/sensor/by/'+falseUUID)
+            .end(function(err, res){
+                res.status.should.equal(400);
+                res.body.should.have.all.keys('status','message','errors');
+                done();
+            });
+    });
+});
+describe('DELETE /api/sensor/by/UUID', function(){
+    it('should return true when given a valid UUID', function(done){
+        chai.request(server)
+            .delete('/api/sensor/by/'+UUID)
+            .end(function(err, res){
+                res.status.should.equal(200);
+                res.body.status.should.be.true;
+                res.body.should.have.all.keys('status','message','dataDeleted');
+                done();
+            });
+    });
+    it('should return not found when given no UUID', function(done){
+        chai.request(server)
+            .delete('/api/sensor/by/')
+            .end(function(err, res){
+                res.status.should.equal(404);
+                done();
+            });
+    });
+    it('should error given an invalid UUID', function(done){
+        chai.request(server)
+            .delete('/api/sensor/by/'+UUID+'1')
+            .end(function(err, res){
+                res.status.should.equal(400);
+                res.body.status.should.be.false;
+                res.body.should.have.all.keys('status','message','errors');
+                res.body.errors[0].should.have.all.keys('param','msg','value');
+                done();
+            });
+    });
+    it('should error when given a UUID that doesnt exist', function(done){
+        chai.request(server)
+            .delete('/api/sensor/by/'+UUID)
+            .end(function(err, res){
+                res.status.should.equal(400);
+                res.body.should.have.all.keys('status','message','errors');
+                done();
+            });
     });
 });
