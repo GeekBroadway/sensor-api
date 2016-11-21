a843b5fbd26f59dd3716c0099e3dc5b627b9f42b
Jack Broadway <jack.c.broadway@gmail.com>
2016-11-21 16:58:52 +1000

    Fixed outdated configs

--- config/config.def.js
+++ config/config.def.js
@@ -1,13 +1,10 @@
 let config = {};
 
-config.filesystem = {};
-config.filesystem.root = "/home/vagrant/sensor_backend/"; //Include the trailing slash
-
 config.server = {};
 config.server.port = '8000';
 config.server.timeout = 5000;
-config.server.sslCertPath = config.filesystem.root+'config/certs/https_cert.pem';
-config.server.sslKeyPath = config.filesystem.root+'config/certs/https_key.pem';
+config.server.sslCertPath = 'config/certs/https_cert.pem';
+config.server.sslKeyPath = 'config/certs/https_key.pem';
 
 config.mongo = {};
 config.mongo.url = "mongodb://localhost/sensorapi";
