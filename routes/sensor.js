6f2532e79eeba8ff67ef3ff94ddb2a9447609638
Jack Broadway <jack.c.broadway@gmail.com>
2016-11-21 15:28:16 +1000

    Updated sensor routes tests

--- routes/sensor.js
+++ routes/sensor.js
@@ -1,40 +1,50 @@
 //modules
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 //Local includes
 let config = require('../config/config.js');
 let logger = require('../utils/logger.js');
 //Setup
 const sensor = express();
 sensor.use(bodyParser.json());
+sensor.use(expressValidator());
 //mongoose
 let Sensor = require('../models/sensor.js');
 //Routes
 sensor.post('/add', postAddSensor);
 //Logic
 function postAddSensor(req, res){
-    if(!req.body.name || !req.body.type || !req.body.desc || !req.body.hub) {
-        res.status(400).json({status: false, message: 'Please pass all body lets'});
-    } else {
-        let senName = req.body.name,
-            senType = req.body.type,
-            senDesc = req.body.desc,
-            senHub = req.body.hub;
-        let newSensor = new Sensor({
-           name: senName,
-           type: senType,
-           desc: senDesc,
-           hub: senHub,
-           data: []
-        });
-        newSensor.save(function(err){
-            if(err) {
-                res.status(500).json({status: false, message: "an error occurred"});
-                logger.error(err);
-            } else {
-                res.json({status: true, message: "sensor added", sensorId: newSensor._id});
-            }
-        });
+    req.checkBody('name', 'Invalid sensorId').notEmpty();
+    req.checkBody('type', 'Invalid sensorData').notEmpty();
+    req.checkBody('desc', 'Invalid sensorData').notEmpty();
+    req.checkBody('hub', 'Invalid sensorData').notEmpty();
+
+    let errors = req.validationErrors();
+    if(errors) {
+        res.status(400).json({status: false, message: 'Val Errors', errors: errors});
+        return;
     }
+
+    let senName = req.body.name,
+        senType = req.body.type,
+        senDesc = req.body.desc,
+        senHub = req.body.hub;
+
+    let newSensor = new Sensor({
+       name: senName,
+       type: senType,
+       desc: senDesc,
+       hub: senHub,
+       data: []
+    });
+    newSensor.save(function(err){
+        if(err) {
+            res.status(500).json({status: false, message: "DB Error occurred", errors: null});
+            logger.error(err);
+        } else {
+            res.json({status: true, message: "sensor added", sensorId: newSensor._id});
+        }
+    });
 }
 module.exports = sensor;
--- test/sensor.js
+++ test/sensor.js
@@ -18,7 +18,84 @@ describe('POST /api/sensor/add', function(){
                "hub": "HUBID"
            }).end(function(err, res){
                 res.status.should.equal(200);
+                res.body.status.should.be.true;
+                res.body.should.have.all.keys('status','message','sensorId');
                 done();
        });
    });
+   it('should error given not given name', function(done){
+       chai.request(server)
+           .post('/api/sensor/add')
+           .send({
+               "type": "Temp & Humidity",
+               "desc": "Living Room Sensor",
+               "hub": "HUBID"
+           }).end(function(err, res){
+           res.status.should.equal(400);
+           res.body.status.should.be.false;
+           res.body.should.have.all.keys('status','message','errors');
+           res.body.errors[0].should.have.all.keys('param','msg');
+           done();
+       });
+   });
+   it('should error given not given type', function(done){
+       chai.request(server)
+           .post('/api/sensor/add')
+           .send({
+               "name": "Sensor 1",
+               "desc": "Living Room Sensor",
+               "hub": "HUBID"
+           }).end(function(err, res){
+           res.status.should.equal(400);
+           res.body.status.should.be.false;
+           res.body.should.have.all.keys('status','message','errors');
+           res.body.errors[0].should.have.all.keys('param','msg');
+           done();
+       });
+   });
+   it('should error given not given desc', function(done){
+       chai.request(server)
+           .post('/api/sensor/add')
+           .send({
+               "name": "Sensor 1",
+               "type": "Temp & Humidity",
+               "hub": "HUBID"
+           }).end(function(err, res){
+           res.status.should.equal(400);
+           res.body.status.should.be.false;
+           res.body.should.have.all.keys('status','message','errors');
+           res.body.errors[0].should.have.all.keys('param','msg');
+           done();
+       });
+   });
+   it('should error given not given hub', function(done){
+       chai.request(server)
+           .post('/api/sensor/add')
+           .send({
+               "name": "Sensor 1",
+               "type": "Temp & Humidity",
+               "desc": "Living Room Sensor"
+           }).end(function(err, res){
+           res.status.should.equal(400);
+           res.body.status.should.be.false;
+           res.body.should.have.all.keys('status','message','errors');
+           res.body.errors[0].should.have.all.keys('param','msg');
+           done();
+       });
+   });
+   it('should error properly not given multiple fields', function(done){
+        chai.request(server)
+            .post('/api/sensor/add')
+            .send({
+                "type": "Temp & Humidity",
+                "desc": "Living Room Sensor"
+            }).end(function(err, res){
+            res.status.should.equal(400);
+            res.body.status.should.be.false;
+            res.body.should.have.all.keys('status','message','errors');
+            res.body.errors[0].should.have.all.keys('param','msg');
+            res.body.errors[1].should.have.all.keys('param','msg');
+            done();
+        });
+    });
 });
